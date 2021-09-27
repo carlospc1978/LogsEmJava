e236131d3a0f9e027dd2702816c3a0c1f7d11546
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-09-27 20:52:23 -0300

    segundo commit

--- src/Principal.java
+++ src/Principal.java
@@ -3,8 +3,9 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] naoprecisaEscreverArgsAqui) {
 
+        //TODO
         String paraForcarErro="X";
 
         try {
