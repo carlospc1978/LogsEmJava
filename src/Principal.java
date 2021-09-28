9ed9b1ebcdee9585eebc0f2854f89c1cbce59ebb
Carlos Cartaxo <carlos.cartaxo@fcamara.com.br>
2021-09-28 08:47:43 -0300

    ajustando classe principal

--- src/Principal.java
+++ src/Principal.java
@@ -3,16 +3,20 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class Principal {
 
-    public static void main(String[] naoprecisaEscreverArgsAqui) {
+    public static void main(String[] args) {
 
         //TODO
         String paraForcarErro="X";
+
+        System.out.println("testes");
 
         try {
             int a = Integer.parseInt(paraForcarErro);
         }catch (Exception ex){
 //            () -> java.util.logging;
-            log.info("");
+            log.info("erro aqui");
+            log.error("erro aqui {} outra {}","primeira","");
+            System.out.println("log");
 
         }
     }
